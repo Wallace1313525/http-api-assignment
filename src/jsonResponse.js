bb55d8531b068601cf5e2d098bc2dcc286b0f284
Wallace Wallace <dbw5808@rit.edu>
2020-02-08 18:23:48 -0500

    implemented JSON

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -1,26 +1,120 @@
-const text = require('./textResponse.js');
+// function to send a json object
+const respondJSON = (request, response, status, object) => {
 
-const getHelloJSON = (request, response) => {
-  const helloJSON = {
-    message: text.hello,
-  };
-  const stringMessage = JSON.stringify(helloJSON);
+  response.writeHead(status, { 'Content-Type': 'application/json' });
 
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.write(stringMessage);
+  response.write(JSON.stringify(object));
+
   response.end();
 };
 
-const getTimeJSON = (request, response) => {
-  const timeJSON = {
-    time: text.getTimeString(),
+//success code
+const success = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'Success!',
   };
-  const stringMessage = JSON.stringify(timeJSON);
 
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.write(stringMessage);
-  response.end();
+  respondJSON(request, response, 200, responseJSON);
 };
 
-module.exports.getHelloJSON = getHelloJSON;
-module.exports.getTimeJSON = getTimeJSON;
+//forbidden code
+const foribidden = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'You do not have access to this content',
+    id: 'forbidden'
+  };
+
+  respondJSON(request, response, 403, responseJSON);
+};
+
+//internal code
+const internal = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'Internal Server Error.  Something went wrong',
+    id: 'Internal Error'
+  };
+
+  respondJSON(request, response, 500, responseJSON);
+};
+
+
+//internal code
+const notImplemented = (request, response) => {
+  // message to send
+  const responseJSON = {
+    message: 'A get request has not yet been implemented.  Check again later',
+    id: 'notImplemented'
+  };
+
+  respondJSON(request, response, 500, responseJSON);
+};
+
+// function to show a bad request without the correct parameters
+const badRequest = (request, response, params) => {
+  // message to send
+  const responseJSON = {
+    message: 'BadRequest',
+  };
+
+  // if the request does not contain a valid=true query parameter
+  if (!params.valid || params.valid !== 'true') {
+    // set our error message
+    responseJSON.message = 'Missing valid query parameter set to true';
+    // give the error a consistent id
+    responseJSON.id = 'badRequest';
+    // return our json with a 400 bad request code
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  // if the parameter is here, send json with a success status code
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const unauthorized = (request, response, params) => {
+  // message to send
+  const responseJSON = {
+    message: 'Unauthorized',
+  };
+
+  // if the request does not contain a valid=true query parameter
+  if (!params.loggedIn || params.loggedIn !== 'yes') {
+    // set our error message
+    responseJSON.message = 'Missing valid query parameter set to yes';
+    // give the error a consistent id
+    responseJSON.id = 'unauthorized';
+    // return our json with a 401 bad request code
+    return respondJSON(request, response, 401, responseJSON);
+  }
+
+  // if the parameter is here, send json with a success status code
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+
+// function to show not found error
+const notFound = (request, response) => {
+  // error message with a description and consistent error id
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return our json with a 404 not found error code
+  respondJSON(request, response, 404, responseJSON);
+};
+
+// exports to set functions to public.
+// In this syntax, you can do getIndex:getIndex, but if they
+// are the same name, you can short handle to just getIndex,
+module.exports = {
+  success,
+  badRequest,
+  notFound,
+  unauthorized,
+  foribidden,
+  internal,
+  notImplemented
+};
--- src/server.js
+++ src/server.js
@@ -1,41 +1,44 @@
-const http = require('http');
-const htmlHandler = require('./htmlResponse');
-const textHandler = require('./textResponse');
-const jsonHandler = require('./jsonResponse');
+
+const http = require('http'); // pull in the http server module
+const url = require('url'); // pull in the url module
+// pull in the query string module
+const query = require('querystring');
+// pull in our html response handler file
+const htmlHandler = require('./htmlResponse.js');
+// pull in our json response handler file
+const jsonHandler = require('./jsonResponse.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = (request, response) => {
-  console.log(request.url);
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+};
 
-  switch (request.url) {
-    case '/':
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/page2':
-      htmlHandler.getPage2(request, response);
-      break;
-    case '/hello':
-      textHandler.getHello(request, response);
-      break;
-    case '/time':
-      textHandler.getTime(request, response);
-      break;
-    case '/helloJSON':
-      jsonHandler.getHelloJSON(request, response);
-      break;
-    case '/timeJSON':
-      jsonHandler.getTimeJSON(request, response);
-      break;
-    case '/dankmemes':
-      htmlHandler.getPNG(request, response);
-      break;
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
+// handle HTTP requests. In node the HTTP server will automatically
+// send this function request and pre-filled response objects.
+const onRequest = (request, response) => {
+  // parse the url using the url module
+  // This will let us grab any section of the URL by name
+  const parsedUrl = url.parse(request.url);
+
+  // grab the query parameters (?key=value&key2=value2&etc=etc)
+  // and parse them into a reusable object by field name
+  const params = query.parse(parsedUrl.query);
+
+  // check if the path name (the /name part of the url) matches
+  // any in our url object. If so call that function. If not, default to index.
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
   }
 };
 
+// start HTTP server
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
+© 2020 GitHub, Inc.
