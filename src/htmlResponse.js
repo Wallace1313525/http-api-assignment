237af0afbd61d56d8085184a7e9f5bc2fbb314f3
Wallace Wallace <dbw5808@rit.edu>
2020-02-07 22:05:23 -0500

    updated css

--- src/htmlResponse.js
+++ src/htmlResponse.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
